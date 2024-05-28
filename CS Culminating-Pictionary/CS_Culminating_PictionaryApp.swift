975141a1cdeaf6047d506d4d2565fa0bbe8ff9fa
Dexter Ng <DNg@lcs.on.ca>
2024-05-28 08:44:40 -0400

    Initial Commit

---
+++ CS Culminating-Pictionary/CS_Culminating_PictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS_Culminating_PictionaryApp.swift
+//  CS Culminating-Pictionary
+//
+//  Created by Dexter Ng on 2024-05-28.
+//
+
+import SwiftUI
+
+@main
+struct CS_Culminating_PictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
